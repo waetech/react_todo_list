df406c6e3b8b0e04af979fa88ade23e800cfd2d7
Roxanne Reyes <waetechsolutions@gmail.com>
2019-07-15 14:36:05 -0400

    First commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+export class TodoItem extends Component {
+    render() {
+        return (
+            <div>
+            <p>Hello</p>
+            </div>
+        )
+    }
+}
+
+export default TodoItem
