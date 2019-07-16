953775c1f80bc62a2894f22fa5505e390d5e8bf8
Roxanne Reyes <waetechsolutions@gmail.com>
2019-07-15 22:54:10 -0400

    Add routers for about page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
 import uuid from 'uuid';
 
 
@@ -54,13 +56,21 @@ class App extends Component {
 render(){
 console.log(this.state.todos)
   return (
+    <Router>
     <div className="App">
     <div className="container">
     <Header />
-    <AddTodo addTodo={this.addTodo} />
+    <Route exact path="/" render={props => (
+      <React.Fragment>
+       <AddTodo addTodo={this.addTodo} />
     <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      </React.Fragment>
+    )} />
+    <Route path="/about" component={About} />
+   
     </div>
     </div>
+    </Router>
   );
 }
     
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 function Header() {
     return (
         <header style={headerStyle}>
             <h1>TodoList</h1>
+            <Link style={linkstyle} to="/">Home</Link> | <Link to="/about" style={linkstyle}>About</Link> 
         </header>
     )
 }
@@ -15,6 +17,11 @@ const headerStyle = {
     textAlign: 'center',
     padding: '10px'
 
+}
+
+const linkstyle = {
+    color: '#fff',
+    textDecoration: 'none'
 }
 
 export default Header
