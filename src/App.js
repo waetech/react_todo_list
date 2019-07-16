718695e2a7c7f2d67f7f14427f5af2b34b951187
Roxanne Reyes <waetechsolutions@gmail.com>
2019-07-15 22:32:52 -0400

    Add uuid random id

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
       title: 'Take out the trash',
       completed: false
       },
       {
-      id: 2,
+      id: uuid.v4(),
       title: 'Meet with the boss',
       completed: true
       },
       {
-      id: 3,
+      id: uuid.v4(),
       title: 'Lunch with husband',
       completed: false
       } 
@@ -40,13 +41,23 @@ class App extends Component {
      this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
     }
 
+    //Add Todo
+    addTodo = (title) => {
+      const newTodo = {
+        id: uuid.v4(),
+        title,
+        completed: false
+      }
+     this.setState({ todos: [...this.state.todos, newTodo] });
+    }
+
 render(){
 console.log(this.state.todos)
   return (
     <div className="App">
     <div className="container">
     <Header />
-    <AddTodo />
+    <AddTodo addTodo={this.addTodo} />
     <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
     </div>
     </div>
